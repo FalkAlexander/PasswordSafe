f46053e54517a817fa8bc85e5da1d8bfd24bcffc
Falk Alexander Seidl <fa@terminal.run>
2018-05-02 20:04:56 +0200

    Added database creation gui

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ import KeepassLoader
 
 fenster = Gtk.Window(title="KeepassGtk")
 
+builder = Gtk.Builder()
+builder.add_from_file("ui/password_creation.glade")
+
 def assembleWindow():
     fenster.connect("destroy", Gtk.main_quit)
     fenster.set_default_size(800, 500)
@@ -20,16 +23,16 @@ def assembleWindow():
     headerbar = builder.get_object("headerbar")
     fenster.set_titlebar(headerbar)
 
-    grid = Gtk.Grid()
-    fenster.add(grid)
+    #grid = Gtk.Grid()
+    #fenster.add(grid)
 
-    button = Gtk.Button(label="Gruppen ausgeben")
-    button.connect("clicked", on_button_clicked)
-    grid.attach(button, 0, 0, 1, 1)
+    #button = Gtk.Button(label="Gruppen ausgeben")
+    #button.connect("clicked", on_button_clicked)
+    #grid.attach(button, 0, 0, 1, 1)
 
-    button2 = Gtk.Button(label="Passwort ändern")
-    button2.connect("clicked", on_password_change)
-    grid.attach(button2, 1, 0, 1, 1)
+    #button2 = Gtk.Button(label="Passwort ändern")
+    #button2.connect("clicked", on_password_change)
+    #grid.attach(button2, 1, 0, 1, 1)
 
     file_open_button = builder.get_object("open_button")
     file_open_button.connect("clicked", open_filechooser)
@@ -52,33 +55,89 @@ def open_filechooser(widget):
 
 def create_filechooser(widget):
     dialog = Gtk.FileChooserDialog("Create new Database", fenster, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-    respone = dialog.run()
-    if respone == Gtk.ResponseType.OK:
+    dialog.set_current_name("Database.kdbx")
+
+    filter_text = Gtk.FileFilter()
+    filter_text.set_name("Keepass 2 Database")
+    filter_text.add_mime_type("application/x-keepass2")
+    dialog.add_filter(filter_text)
+
+    response = dialog.run()
+    if response == Gtk.ResponseType.OK:
         print("Saving..." + dialog.get_filename())
         shutil.copy2('data/database.kdbx', dialog.get_filename())
         dialog.close()
-    elif respone == Gtk.ResponseType.CANCEL:
+        print("Setze Datenbank Pfad: " + dialog.get_filename())
+        print("Bekomme Datenbank Pfad: " + KeepassLoader.get_database())
+        KeepassLoader.set_database(dialog.get_filename())
+        password_creation()
+    elif response == Gtk.ResponseType.CANCEL:
         print("Database creation canceled")
         dialog.close()
 
+#def get_widget(file, widget):
+#    builder = Gtk.Builder()
+#    builder.add_from_file(file)
+#    widget = builder.get_object(widget)
+#    return widget
 
-def on_button_clicked(widget):
-    print("Gruppen ausgeben")
-    KeepassLoader.readDB()
-    KeepassLoader.addGroup()
-    KeepassLoader.addEntry()
-    KeepassLoader.printGroup()
-    KeepassLoader.saveGroup()
+def password_creation():
+    stack = builder.get_object("database_creation_stack")
+    stack.set_visible_child(stack.get_child_by_name("page0"))
+    password_creation_button = builder.get_object("password_creation_button")
+    password_creation_button.connect("clicked", on_password_creation_button_clicked)
+    fenster.add(stack)
 
-def on_password_change(widget):
-    print("Passwort ändern")
-    KeepassLoader.changePassword()
+def on_password_creation_button_clicked(widget):
+    password_creation_input = builder.get_object("password_creation_input")
+    KeepassLoader.set_password_try(password_creation_input.get_text())
 
-def get_widget(file, widget):
-    builder = Gtk.Builder()
-    builder.add_from_file(file)
-    widget = builder.get_object(widget)
-    return widget
+    check_password_page()
+
+def check_password_page():
+    stack = builder.get_object("database_creation_stack")
+    stack.set_visible_child(stack.get_child_by_name("page1"))
+
+    password_check_button = builder.get_object("password_check_button")
+    password_check_button.connect("clicked", on_password_check_button_clicked)
+
+def on_password_check_button_clicked(widget):
+    password_check_input = builder.get_object("password_check_input")
+    KeepassLoader.set_password_check(password_check_input.get_text())
+
+    if KeepassLoader.compare_passwords:
+        success_page()
+    else:
+        repeat_page()
+
+def success_page():
+    print("Datenbank Pfad: " + KeepassLoader.get_database())
+    KeepassLoader.set_database_password(KeepassLoader.get_database(), KeepassLoader.password_check)
+
+    stack = builder.get_object("database_creation_stack")
+    stack.set_visible_child(stack.get_child_by_name("page3"))
+
+def repeat_page():
+    stack = builder.get_object("database_creation_stack")
+    stack.set_visible_child(stack.get_child_by_name("page2"))
+
+    password_repeat_button = builder.get_object("password_repeat_button")
+    password_repeat_button.connect("clicked", on_password_repeat_button_clicked)
+
+def on_password_repeat_button_clicked(widget):
+    password_repeat_input1 = builder.get_object("password_repeat_input1")
+    password_repeat_input2 = builder.get_object("password_repeat_input2")
+
+    KeepassLoader.set_password_try(password_repeat_input1.get_text())
+    KeepassLoader.set_password_check(password_repeat_input2.get_text())
+
+    if KeepassLoader.compare_passwords():
+        success_page()
+    else:
+        password_repeat_input1.set_text("")
+        password_repeat_input2.set_text("")
+        password_repeat_input1.get_style_context().add_class("error")
+        password_repeat_input2.get_style_context().add_class("error")
 
 def config():
     HOME = os.getenv("HOME")

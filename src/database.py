2d24e4bc2562119c8be3aaeb4e99c11f2c94d070
baumkuchen42 <uta@terminal.run>
2018-05-03 10:00:53 +0200

    more objectorientation in KeepassLoader

--- src/database.py
+++ src/database.py
@@ -25,48 +25,50 @@ class KeepassLoader:
     def add_entry():
         entry = Entry(group_path,entry_name,username,password,url,note,icon)
         self.kp.add_entry(self.kp.find_groups_by_path(entry.get_group_path)), entry.get_entry_name(), entry.get_username(), entry.get_password(), url=entry.get_url(), note=entry.get_note(), icon=entry.get_icon())
-        #####stopped here##########################
 
-    def print_group():
-        print(group.entries)
+    def get_entries(self, group_path):
+        group = self.kp.find_groups_by_path(group_path)
+        entry_list = []
+        for entry in group.entries:
+            entry_list.append(entry)
 
-    def save():
-        kp.save()
+        return entry_list
 
-    def set_database_password(password):
-        kp.set_credentials(password)
-        save_database()
 
-    def change_database_password(database, old_password, new_password):
+    def save(self):
+        self.kp.save()
+
+    def set_database_password(self, password):
+        self.kp.set_credentials(password)
+        self.password = password
+        save()
+
+    def change_database_password(self, old_password, new_password):
         kp = PyKeePass(database, old_password)
-        read_database()
+        read_database() #this is still with the initial password
         kp.set_credentials(new_password)
-        save_database()
+        save()
 
-    def set_database(path):
-        global database_path
-        database_path = path
+    def set_database(self, path):
+        self.database_path = path
 
-        global kp
-        kp = PyKeePass(database_path, password='liufhre86ewoiwejmrcu8owe')
+        self.kp = PyKeePass(self.database_path, password='liufhre86ewoiwejmrcu8owe') #this applies also just for initial configuration
 
-    def get_database():
-        return database_path
+    def get_database(self):
+        return self.database_path
 
-    def set_password_try(password):
-        global password_try
-        password_try = password
+    def set_password_try(self, password):
+        self.password_try = password
 
-    def set_password_check(password):
-        global password_check
-        password_check = password
+    def set_password_check(self, password):
+        self.password_check = password
 
     def compare_passwords():
-        print(password_try)
-        print(password_check)
-        if password_try == password_check:
-            if password_try == "" and password_check == "":
-                print("false verkettet")
+        print(self.password_try)
+        print(self.password_check)
+        if self.password_try == self.password_check:
+            if self.password_try == "" and self.password_check == "":
+                print("empty password")
                 return False
             else:
                 print("true")

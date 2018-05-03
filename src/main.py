71f3e648897d5110a9f96526701b3821a88186b1
baumkuchen42 <uta@terminal.run>
2018-05-03 17:34:45 +0200

    more object orientation for main.py (class MainWindow)

--- src/main.py
+++ src/main.py
@@ -8,51 +8,47 @@ import database
 from database import KeepassLoader
 import config
 
-fenster = Gtk.Window(title="KeepassGtk")
-
-builder = Gtk.Builder()
-builder.add_from_file("ui/password_creation.glade")
-
 class MainWindow(Gtk.Window):
+
+    keepass_loader = NotImplemented
 
     def __init__(self):
         config.configure()
-        self.assembleWindow()
-        fenster.show_all()
-        Gtk.main()
+        self.assemble_window()
 
 
-    def assembleWindow():
-        fenster.connect("destroy", Gtk.main_quit)
-        fenster.set_default_size(800, 500)
+    def assemble_window(self):
+        Gtk.Window.__init__(self, title="KeepassGtk")
+        self.connect("destroy", Gtk.main_quit)
+        self.set_default_size(800, 500)
 
         builder = Gtk.Builder()
         builder.add_from_file("ui/headerbar.glade")
-
+        
         headerbar = builder.get_object("headerbar")
-        fenster.set_titlebar(headerbar)
+        self.set_titlebar(headerbar)
 
         file_open_button = builder.get_object("open_button")
-        file_open_button.connect("clicked", open_filechooser)
+        file_open_button.connect("clicked", self.open_filechooser)
 
         file_new_button = builder.get_object("new_button")
-        file_new_button.connect("clicked", create_filechooser)
+        file_new_button.connect("clicked", self.create_filechooser)
 
 
     # Events
 
-    def open_filechooser(widget):
-        dialog = Gtk.FileChooserDialog("Choose Keepass Database", fenster, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+    def open_filechooser(self, widget):
+        dialog = Gtk.FileChooserDialog("Choose Keepass Database", self, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
             print("File selected: " + dialog.get_filename())
             dialog.close()
         elif response == Gtk.ResponseType.CANCEL:
-            print("File selection canceled")
+            print("File selection cancelled")
             dialog.close()
 
-    def create_filechooser(widget):
-        dialog = Gtk.FileChooserDialog("Create new Database", fenster, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+    def create_filechooser(self, widget):
+        dialog = Gtk.FileChooserDialog("Create new Database", self, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
         dialog.set_current_name("Database.kdbx")
 
         filter_text = Gtk.FileFilter()
@@ -66,33 +62,30 @@ class MainWindow(Gtk.Window):
             shutil.copy2('data/database.kdbx', dialog.get_filename())
             dialog.close()
             print("Setze Datenbank Pfad: " + dialog.get_filename())
-            KeepassLoader.set_database(dialog.get_filename())
-            print("Bekomme Datenbank Pfad: " + KeepassLoader.get_database())
-            password_creation()
+            self.keepass_loader = KeepassLoader(dialog.get_filename(), "liufhre86ewoiwejmrcu8owe")
+            print("Bekomme Datenbank Pfad: " + self.keepass_loader.get_database())
+            self.password_creation()
         elif response == Gtk.ResponseType.CANCEL:
-            print("Database creation canceled")
+            print("Database creation cancelled")
             dialog.close()
 
-    #def get_widget(file, widget):
-    #    builder = Gtk.Builder()
-    #    builder.add_from_file(file)
-    #    widget = builder.get_object(widget)
-    #    return widget
+    def password_creation(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("ui/database_creation.glade")
 
-    def password_creation():
         stack = builder.get_object("database_creation_stack")
         stack.set_visible_child(stack.get_child_by_name("page0"))
         password_creation_button = builder.get_object("password_creation_button")
-        password_creation_button.connect("clicked", on_password_creation_button_clicked)
-        fenster.add(stack)
+        password_creation_button.connect("clicked", self.on_password_creation_button_clicked)
+        self.add(stack)
 
-    def on_password_creation_button_clicked(widget):
+    def on_password_creation_button_clicked(self, widget):
         password_creation_input = builder.get_object("password_creation_input")
-        KeepassLoader.set_password_try(password_creation_input.get_text())
+        self.keepass_loader.set_password_try(password_creation_input.get_text())
 
-        check_password_page()
+        self.check_password_page()
 
-    def check_password_page():
+    def check_password_page(self):
         stack = builder.get_object("database_creation_stack")
         stack.set_visible_child(stack.get_child_by_name("page1"))
 
@@ -152,3 +145,6 @@ class MainWindow(Gtk.Window):
         password_repeat_input1.set_text("")
         password_repeat_input2.set_text("")
 
+main_window = MainWindow()
+main_window.show_all()
+Gtk.main()

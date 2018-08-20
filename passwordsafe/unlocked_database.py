17168f77d1fe97545e318debfb1ef39ae2132783
Falk Alexander Seidl <fa@terminal.run>
2018-08-20 15:49:21 +0200

    Fix crash when typing fast in search

--- passwordsafe/unlocked_database.py
+++ passwordsafe/unlocked_database.py
@@ -46,6 +46,7 @@ class UnlockedDatabase:
     database_locked = False
     listbox_insert_thread = NotImplemented
     result_list = NotImplemented
+    search_thread = NotImplemented
 
     entry_marked_for_delete = NotImplemented
     group_marked_for_delete = NotImplemented
@@ -1224,9 +1225,9 @@ class UnlockedDatabase:
         if search_fulltext_button.get_active() is True:
             fulltext = True
 
-        search_thread = threading.Thread(target=self.search_thread_creation, args=(search_local_button, widget, fulltext, result_list, empty_search_overlay, info_search_overlay))
-        search_thread.daemon = True
-        search_thread.start()
+        self.search_thread = threading.Thread(target=self.search_thread_creation, args=(search_local_button, widget, fulltext, result_list, empty_search_overlay, info_search_overlay))
+        self.search_thread.daemon = False
+        self.search_thread.start()
 
     def search_thread_creation(self, search_local_button, widget, fulltext, result_list, empty_search_overlay, info_search_overlay):
         if search_local_button.get_active() is True:

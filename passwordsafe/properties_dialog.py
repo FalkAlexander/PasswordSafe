4b3a20450d4fd9418be96fb9fd2e3a4f4e871316
Falk Alexander Seidl <fseidl@gnome.org>
2018-10-15 21:00:56 +0200

    Add and Connect Properties Dialog

---
+++ passwordsafe/properties_dialog.py
@@ -0,0 +1,40 @@
+from gi.repository import Gtk
+
+
+class PropertiesDialog:
+    dialog = NotImplemented
+
+    unlocked_database = NotImplemented
+    database_manager = NotImplemented
+    builder = NotImplemented
+
+    def __init__(self, unlocked_database):
+        self.unlocked_database = unlocked_database
+        self.database_manager = unlocked_database.database_manager
+        self.builder = Gtk.Builder()
+        self.builder.add_from_resource("/org/gnome/PasswordSafe/properties_dialog.ui")
+
+        self.assemble_dialog()
+
+    def assemble_dialog(self):
+        self.dialog = self.builder.get_object("properties_dialog")
+
+        # Dialog
+        self.dialog.set_modal(True)
+        self.dialog.set_transient_for(self.unlocked_database.window)
+        self.dialog.present()
+
+        self.unlocked_database.references_dialog = self.dialog
+        self.dialog.connect("delete-event", self.on_dialog_quit)
+
+        self.update_properties()
+
+    def update_properties(self):
+        return
+
+    #
+    # Tools
+    #
+
+    def on_dialog_quit(self, window, event):
+        self.unlocked_database.references_dialog = NotImplemented

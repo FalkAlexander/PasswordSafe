03b2d093e68ddb2d3a08e6d95e5458d57508ac56
Falk Alexander Seidl <fseidl@gnome.org>
2018-11-11 14:33:29 +0100

    Add Search Button and Search Bar
    Add a button which reveales a search bar to the notes dialog.

--- passwordsafe/notes_dialog.py
+++ passwordsafe/notes_dialog.py
@@ -7,6 +7,10 @@ class NotesDialog():
     unlocked_database = NotImplemented
     builder = NotImplemented
 
+    search_button = NotImplemented
+    search_bar = NotImplemented
+    search_entry = NotImplemented
+
     def __init__(self, unlocked_database):
         self.unlocked_database = unlocked_database
         self.builder = Gtk.Builder()
@@ -28,8 +32,16 @@ class NotesDialog():
         scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
         scrolled_page.notes_dialog_value_entry = self.builder.get_object("value_entry")
         scrolled_page.notes_dialog_value_entry.get_buffer().connect("changed", self.on_value_entry_changed)
+
         self.builder.get_object("copy_button").connect("clicked", self.on_copy_button_clicked)
         self.builder.get_object("close_button").connect("clicked", self.on_close_button_clicked)
+
+        # Search
+        self.search_button = self.builder.get_object("search_button")
+        self.search_bar = self.builder.get_object("search_bar")
+        self.search_entry = self.builder.get_object("search_entry")
+
+        self.search_button.connect("toggled", self.on_search_button_toggled)
 
         self.update_value_entry()
 
@@ -66,6 +78,17 @@ class NotesDialog():
 
     def on_close_button_clicked(self, button):
         self.dialog.destroy()
+
+    def on_search_button_toggled(self, button):
+        self.toggle_search_bar()
+
+    def toggle_search_bar(self):
+        if self.search_bar.get_search_mode() is True:
+            self.search_bar.set_search_mode(False)
+            self.search_button.set_active(False)
+        else:
+            self.search_bar.set_search_mode(True)
+            self.search_button.set_active(True)
 
     #
     # Tools

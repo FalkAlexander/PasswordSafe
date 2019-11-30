996bb8cdbc9fa2bdbad38da2e9b04900dbd9b956
Jean Felder <jean.felder@gmail.com>
2019-11-30 02:12:48 +0100

    unlocked_database: Remove empty line at end of file

--- passwordsafe/unlocked_database.py
+++ passwordsafe/unlocked_database.py
@@ -933,4 +933,3 @@ class UnlockedDatabase:
     def unregister_dbus_signal(self):
         app = Gio.Application.get_default
         app().get_dbus_connection().signal_unsubscribe(self.dbus_subscription_id)
-

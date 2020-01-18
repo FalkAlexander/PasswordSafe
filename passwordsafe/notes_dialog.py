54dcd2c876c8588cfab0d762afff3a5b1aea4e5d
Falk Alexander Seidl <fa@terminal.run>
2020-01-18 17:29:54 +0100

    Fix Notes Dialog UUID Handling

--- passwordsafe/notes_dialog.py
+++ passwordsafe/notes_dialog.py
@@ -32,7 +32,7 @@ class NotesDialog():
         self.unlocked_database.references_dialog = self.dialog
         self.dialog.connect("delete-event", self.on_dialog_quit)
 
-        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
+        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group).urn)
         scrolled_page.notes_dialog_value_entry = self.builder.get_object("value_entry")
         scrolled_page.notes_dialog_value_entry.get_buffer().connect("changed", self.on_value_entry_changed)
 
@@ -63,7 +63,7 @@ class NotesDialog():
         self.search_button.add_accelerator("clicked", self.accelerators, key, mod, Gtk.AccelFlags.VISIBLE)
 
     def update_value_entry(self):
-        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
+        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group).urn)
 
         buffer_text = scrolled_page.notes_property_value_entry.get_buffer().get_text(
             scrolled_page.notes_property_value_entry.get_buffer().get_start_iter(),
@@ -77,14 +77,14 @@ class NotesDialog():
 
     def on_value_entry_changed(self, widget):
         self.unlocked_database.start_database_lock_timer()
-        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
+        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group).urn)
 
         scrolled_page.notes_property_value_entry.get_buffer().set_text(
             widget.get_text(widget.get_start_iter(), widget.get_end_iter(), False)
         )
 
     def on_copy_button_clicked(self, button):
-        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
+        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group).urn)
 
         self.unlocked_database.send_to_clipboard(
             scrolled_page.notes_dialog_value_entry.get_buffer().get_text(
@@ -108,7 +108,7 @@ class NotesDialog():
             self.search_bar.set_search_mode(True)
 
     def on_search_entry_changed(self, entry):
-        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
+        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group).urn)
         notes_buffer = scrolled_page.notes_dialog_value_entry.get_buffer()
 
         scrolled_page.notes_dialog_value_entry.get_buffer().remove_all_tags(
@@ -140,6 +140,6 @@ class NotesDialog():
 
     def on_dialog_quit(self, window, event):
         self.unlocked_database.notes_dialog = NotImplemented
-        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group))
+        scrolled_page = self.unlocked_database.stack.get_child_by_name(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_group).urn)
         scrolled_page.notes_dialog_value_entry = NotImplemented
 

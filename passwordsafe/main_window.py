FalkAlexander/PasswordSafe

commit 6ae20f4ff27c940b29333b95f0cc6a3db35d5b61

main_window: Simplify responsive_listener

passwordsafe/main_window.py
@@ -116,14 +116,8 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def responsive_listener(self, win: Gtk.ApplicationWindow) -> None:
         """invoked on check-resize events"""
-        if self.get_allocation().width < 700:
-            if not self.mobile_width:
-                self.mobile_width = True
-                self.change_layout()
-        else:
-            if self.mobile_width:
-                self.mobile_width = False
-                self.change_layout()
+        self.mobile_width = (self.get_allocation().width < 700)
+        self.change_layout()
 
     def change_layout(self):
         """Switches all open databases between mobile/desktop layout"""

commit 3a1774182eb4d6ed9dd2d1ed861af6b14f313597

main_window: Do not change layout if mobile_width has not changed
change_layout only needs to be updated when the mobile_width value has
changed.

This fixes a regression introduced by commit
6ae20f4ff27c940b29333b95f0cc6a3db35d5b61.

passwordsafe/main_window.py
@@ -116,8 +116,10 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def responsive_listener(self, win: Gtk.ApplicationWindow) -> None:
         """invoked on check-resize events"""
+        previous_mobile_width = self.mobile_width
         self.mobile_width = (self.get_allocation().width < 700)
-        self.change_layout()
+        if previous_mobile_width != self.mobile_width:
+            self.change_layout()
 
     def change_layout(self):
         """Switches all open databases between mobile/desktop layout"""
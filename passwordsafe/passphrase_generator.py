3a4a3b2dd3b0e2323939aeb5630efdc87f6fc4c2
Sebastian Spaeth <Sebastian@SSpaeth.de>
2020-11-06 10:35:29 +0100

    passphrase_generator: Encapsulate in a class
    Rather than simply using a global function, we encapsulate the
    generate() phassphrase function in a class method of used to create
    class Passphrase. Much easier to import too, than the previous:
    
    from passwordsafe.passphrase_generator import generate as generate_passphrase
    
    This gives us a Passphrase object which I plan to make a special case of a
    Password class (to be created) which can contain lots of methods that are
    specific to passwords (e.g. compare()'ing 2 passwords)

--- passwordsafe/passphrase_generator.py
+++ passwordsafe/passphrase_generator.py
@@ -1,31 +1,42 @@
 # SPDX-License-Identifier: GPL-3.0-only
+"""Generate a passphrase from a list of predefined words."""
 import logging
 from secrets import randbelow
 from typing import List
 from gi.repository import Gio, GLib
 
 
-def generate(num_words: int, separator: str = "-") -> str:
-    """Generate a passphrase.
+class Passphrase:
+    """Generate a passphrase from a list of predefined words."""
 
-    :param int num_words: number of words requested
-    :param str separator: separator
-    :returns: a passphrase
-    :rtype: str
+    def __init__(self, password: str = None):
+        """password is the (optional) password to be stored"""
+        self.password = password
 
-    """
-    word_file: Gio.File = Gio.File.new_for_uri(
-        "resource:///org/gnome/PasswordSafe/crypto/eff_large_wordlist.txt"
-    )
-    f_buffer: GLib.Bytes = Gio.InputStream.read_bytes(word_file.read(), 108800)
-    word_str: str = f_buffer.get_data().decode("utf-8")
+    @classmethod
+    def generate(cls, num_words: int, separator: str = "-") -> "Passphrase":
+        """Generate a passphrase.
 
-    word_list: List[str] = word_str.split("\n")
-    len_words: int = len(word_list)
+        :param int num_words: number of words requested
+        :param str separator: separator
+        :returns: a Passphrase() object
+        :rtype: `Passphrase`
+        """
+        word_file: Gio.File = Gio.File.new_for_uri(
+            "resource:///org/gnome/PasswordSafe/crypto/eff_large_wordlist.txt"
+        )
+        f_buffer: GLib.Bytes = Gio.InputStream.read_bytes(word_file.read(),
+                                                          108800)
+        word_str: str = f_buffer.get_data().decode("utf-8")
+        word_list: List[str] = word_str.split("\n")
+        len_words: int = len(word_list)
 
-    rand_words = [word_list[randbelow(len_words)] for _ in range(0, num_words)]
-    passphrase: str = separator.join(rand_words)
-    logging.debug(
-        "Created passphrase with %d words and separator '%s'", num_words, separator
-    )
-    return passphrase
+        words = [word_list[randbelow(len_words)] for _ in range(0, num_words)]
+        passphrase: str = separator.join(words)
+        logging.debug("Created passphrase with %d words and separator '%s'",
+                      num_words, separator)
+        return cls(passphrase)
+
+    def __str__(self):
+        """String representation of the passphrase"""
+        return self.password or ""

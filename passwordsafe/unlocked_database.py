FalkAlexander/PasswordSafe

commit e0196fc8f494b6be5c2cbca425ef2d88e79e8cb9

database_manager: Remove get_entry_uuid_from_entry_object usage
entry.uuid can directly be used.

passwordsafe/unlocked_database.py
@@ -315,7 +315,7 @@ class UnlockedDatabase(GObject.GObject):
                 self.set_browser_headerbar()
             # For entry
             else:
-                self._stack.set_visible_child_name(self.database_manager.get_entry_uuid_from_entry_object(self.current_element).urn)
+                self._stack.set_visible_child_name(self.current_element.uuid.urn)
                 self.entry_page.set_entry_page_headerbar()
 
     def add_page(self, scrolled_window: ScrolledPage, name: str) -> None:
@@ -520,9 +520,11 @@ class UnlockedDatabase(GObject.GObject):
         self.builder.get_object("menubutton_popover").popdown()
         self.start_database_lock_timer()
         self.database_manager.is_dirty = True
-        entry = self.database_manager.add_entry_to_database("", "", "", None, None, "0", self.current_element.uuid)
+        elt_uuid = self.current_element.uuid
+        entry = self.database_manager.add_entry_to_database(
+            "", "", "", None, None, "0", elt_uuid)
         self.current_element = entry
-        self.pathbar.add_pathbar_button_to_pathbar(self.database_manager.get_entry_uuid_from_entry_object(self.current_element))
+        self.pathbar.add_pathbar_button_to_pathbar(elt_uuid)
         self.show_page_of_new_directory(False, True)
 
     def on_add_group_button_clicked(self, _param: None) -> None:

passwordsafe/database_manager.py
@@ -171,10 +171,6 @@ class DatabaseManager(GObject.GObject):
     def get_entry_object_from_uuid(self, uuid):
         return self.db.find_entries(uuid=uuid, first=True)
 
-    # Return entry uuid from entry object
-    def get_entry_uuid_from_entry_object(self, entry):
-        return entry.uuid
-
     def get_entry_name(self, data: Union[Entry, UUID]) -> str:
         """Get entry name from an uuid or an entry
 

passwordsafe/entry_page.py
@@ -65,7 +65,7 @@ class EntryPage:
 
         builder.add_from_resource("/org/gnome/PasswordSafe/entry_page.ui")
 
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
         scrolled_page = self.unlocked_database.get_current_page()
 
         if self.unlocked_database.database_manager.has_entry_name(entry_uuid) is True or add_all is True:
@@ -377,7 +377,7 @@ class EntryPage:
 
     def on_property_value_entry_changed(self, widget, type_name):
         self.unlocked_database.start_database_lock_timer()
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
 
         scrolled_page = self.unlocked_database.get_current_page()
         scrolled_page.is_dirty = True
@@ -385,7 +385,7 @@ class EntryPage:
         if type_name == "name":
             self.unlocked_database.database_manager.set_entry_name(entry_uuid, widget.get_text())
 
-            pathbar_button = self.unlocked_database.pathbar.get_pathbar_button(self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element))
+            pathbar_button = self.unlocked_database.pathbar.get_pathbar_button(entry_uuid)
             pathbar_button.set_label(widget.get_text())
 
         elif type_name == "username":
@@ -407,7 +407,7 @@ class EntryPage:
             return
 
         self.unlocked_database.start_database_lock_timer()
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
         scrolled_page = self.unlocked_database.get_current_page()
 
         if str(self.unlocked_database.database_manager.get_icon(entry_uuid)) == button.get_name():
@@ -432,7 +432,7 @@ class EntryPage:
     #
 
     def on_attributes_add_button_clicked(self, _widget):
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
         scrolled_page = self.unlocked_database.get_current_page()
 
         key = scrolled_page.attributes_key_entry.get_text()
@@ -457,7 +457,7 @@ class EntryPage:
         scrolled_page.is_dirty = True
 
     def on_attribute_remove_button_clicked(self, button):
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
         scrolled_page = self.unlocked_database.get_current_page()
 
         parent = button.get_parent().get_parent().get_parent()
@@ -467,7 +467,7 @@ class EntryPage:
         scrolled_page.properties_list_box.remove(parent)
 
     def on_attributes_value_entry_changed(self, widget):
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
 
         parent = widget.get_parent().get_parent().get_parent()
         key = parent.get_name()
@@ -475,7 +475,7 @@ class EntryPage:
         self.unlocked_database.database_manager.set_entry_attribute(entry_uuid, key, widget.get_text())
 
     def on_attribute_key_edit_button_clicked(self, button):
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
 
         parent = button.get_parent().get_parent().get_parent()
         key = parent.get_name()
@@ -544,7 +544,7 @@ class EntryPage:
         response = select_dialog.run()
 
         if response == Gtk.ResponseType.ACCEPT:
-            entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+            entry_uuid = self.unlocked_database.current_element.uuid
             for uri in select_dialog.get_uris():
                 attachment = Gio.File.new_for_uri(uri)
                 byte_buffer = attachment.load_bytes()[0].get_data()
@@ -552,7 +552,7 @@ class EntryPage:
                 self.add_attachment_row(self.unlocked_database.database_manager.add_entry_attachment(entry_uuid, byte_buffer, filename))
 
     def add_attachment_row(self, attachment):
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
         scrolled_page = self.unlocked_database.get_current_page()
 
         builder = Gtk.Builder()
@@ -602,7 +602,7 @@ class EntryPage:
         builder = Gtk.Builder()
         builder.add_from_resource("/org/gnome/PasswordSafe/entry_page.ui")
 
-        entry_uuid = self.unlocked_database.database_manager.get_entry_uuid_from_entry_object(self.unlocked_database.current_element)
+        entry_uuid = self.unlocked_database.current_element.uuid
         scrolled_page = self.unlocked_database.get_current_page()
 
         for row in scrolled_page.attachment_list_box.get_children():

passwordsafe/entry_row.py
@@ -29,7 +29,7 @@ class EntryRow(Gtk.ListBoxRow):
         self.unlocked_database = unlocked_database
         self.database_manager = dbm
 
-        self.entry_uuid = dbm.get_entry_uuid_from_entry_object(entry)
+        self.entry_uuid = entry.uuid
         self.icon = dbm.get_icon(entry)
         self.label = dbm.get_entry_name(entry)
         self.password = dbm.get_entry_password(entry)

passwordsafe/references_dialog.py
@@ -50,8 +50,7 @@ class ReferencesDialog():
 
     def update_reference_entry(self):
         """Update the reference entry and selected label text."""
-        uuid = self.database_manager.get_entry_uuid_from_entry_object(
-            self.unlocked_database.current_element)
+        uuid = self.unlocked_database.current_element.uuid
         encoded_uuid = uuid.hex.upper()
 
         self.builder.get_object("selected_property_label").set_text(self.property)

commit f0fc3aee949ec0a5dc865328ffb3d9d323431293

unlocked_database: Pass the new entry to pathbar on entry creation
Fixes: e0196fc8f494b6be5c2cbca425ef2d88e79e8cb9

passwordsafe/unlocked_database.py
@@ -520,11 +520,10 @@ class UnlockedDatabase(GObject.GObject):
         self.builder.get_object("menubutton_popover").popdown()
         self.start_database_lock_timer()
         self.database_manager.is_dirty = True
-        elt_uuid = self.current_element.uuid
         new_entry: Entry = self.database_manager.add_entry_to_database(
-            "", "", "", None, None, "0", elt_uuid)
+            "", "", "", None, None, "0", self.current_element.uuid)
         self.current_element = new_entry
-        self.pathbar.add_pathbar_button_to_pathbar(elt_uuid)
+        self.pathbar.add_pathbar_button_to_pathbar(new_entry.uuid)
         self.show_page_of_new_directory(False, True)
 
     def on_add_group_button_clicked(self, _param: None) -> None:
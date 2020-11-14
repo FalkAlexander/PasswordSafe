FalkAlexander/PasswordSafe

commit f7c5bde2e83dbdaffc503c78cf9ee1b5d2bf6b8f

selection_ui: Do not remove or add linkedbox_right widget
There is no need to remove or add linkedbox_right when selection-mode
is changed: it only needs to be hidden or shown. By
binding selection-mode property from UnlockedDatabase with its
visibility property, it is possible to achieve the same effect.

passwordsafe/unlocked_database.py
@@ -94,6 +94,7 @@ class UnlockedDatabase(GObject.GObject):
 
         self._current_element: Optional[Union[Entry, Group]] = None
 
+        self._linkedbox_right: Optional[Gtk.Box] = None
         self._selection_button_box: Optional[Gtk.Box] = None
 
         # Declare database as opened
@@ -195,6 +196,12 @@ class UnlockedDatabase(GObject.GObject):
             "selection-mode", self._selection_button_box, "visible",
             GObject.BindingFlags.SYNC_CREATE)
 
+        self._linkedbox_right = self.builder.get_object("linkedbox_right")
+        self.bind_property(
+            "selection-mode", self._linkedbox_right, "visible",
+            GObject.BindingFlags.INVERT_BOOLEAN
+            | GObject.BindingFlags.SYNC_CREATE)
+
         self.parent_widget.set_headerbar(self.headerbar)
         self.window.set_titlebar(self.headerbar)
         self.pathbar = Pathbar(self, self.database_manager, self.database_manager.get_root_group())
@@ -203,8 +210,6 @@ class UnlockedDatabase(GObject.GObject):
 
     # Group and entry browser headerbar
     def set_browser_headerbar(self):
-        self.builder.get_object("linkedbox_right").show_all()
-
         filename_label = self.builder.get_object("filename_label")
         filename_label.set_text(ntpath.basename(self.database_manager.database_path))
 

passwordsafe/selection_ui.py
@@ -70,11 +70,7 @@ class SelectionUI:
         selection_options_button = self.unlocked_database.builder.get_object("selection_options_button")
 
         title_box = self.unlocked_database.builder.get_object("title_box")
-        headerbar_right_box = self.unlocked_database.builder.get_object("headerbar_right_box")
 
-        linkedbox_right = self.unlocked_database.builder.get_object("linkedbox_right")
-
-        headerbar_right_box.remove(linkedbox_right)
         title_box.add(selection_options_button)
 
         context = self.unlocked_database.headerbar.get_style_context()
@@ -103,11 +99,7 @@ class SelectionUI:
         selection_options_button = self.unlocked_database.builder.get_object("selection_options_button")
 
         title_box = self.unlocked_database.builder.get_object("title_box")
-        headerbar_right_box = self.unlocked_database.builder.get_object("headerbar_right_box")
 
-        linkedbox_right = self.unlocked_database.builder.get_object("linkedbox_right")
-
-        headerbar_right_box.add(linkedbox_right)
         title_box.remove(selection_options_button)
         self.unlocked_database.headerbar.set_show_close_button(True)
 

commit fba4d46bff35eeada5628b41fcc0d219c1b5360b

unlocked_database: Correctly set linkedbox_right visibility
The _linkedbox_right widget needs to be hidden when an entry or a group
is edited because a secondary menu becomes visible. When returning the
parent group, it needs to be visible again. This used to work reliably
but the selection-mode rework introduced this regression.

Fix the issue by showing _linkedbox_right when the main headerbar is
restored.

Fixes: f7c5bde2e83dbdaffc503c78cf9ee1b5d2bf6b8f
Closes: #234

passwordsafe/entry_page.py
@@ -31,8 +31,6 @@ class EntryPage:
 
     # Entry creation/editing page headerbar
     def set_entry_page_headerbar(self):
-        self.unlocked_database.builder.get_object("linkedbox_right").hide()
-
         filename_label = self.unlocked_database.builder.get_object("filename_label")
         name = self.unlocked_database.database_manager.get_entry_name(
             self.unlocked_database.current_element)

passwordsafe/group_page.py
@@ -23,8 +23,6 @@ class GroupPage:
 
     # Group creation/editing headerbar
     def set_group_edit_page_headerbar(self):
-        self.unlocked_database.builder.get_object("linkedbox_right").hide()
-
         filename_label = self.unlocked_database.builder.get_object("filename_label")
         group_name = self.unlocked_database.database_manager.get_group_name(
             self.unlocked_database.current_element)

passwordsafe/unlocked_database.py
@@ -217,6 +217,8 @@ class UnlockedDatabase(GObject.GObject):
 
     # Group and entry browser headerbar
     def set_browser_headerbar(self):
+        self._linkedbox_right.show()
+
         filename_label = self.builder.get_object("filename_label")
         filename_label.set_text(ntpath.basename(self.database_manager.database_path))
 
@@ -265,6 +267,7 @@ class UnlockedDatabase(GObject.GObject):
             self.add_page(scrolled_window, self.current_element.uuid.urn)
             self.switch_page(self.current_element)
             self.group_page.insert_group_properties_into_listbox(scrolled_window.properties_list_box)
+            self._linkedbox_right.hide()
             self.group_page.set_group_edit_page_headerbar()
         # If the stack page with current group's uuid isn't existing - we need to create it (first time opening of group/entry)
         elif (not self._stack.get_child_by_name(self.current_element.uuid.urn)
@@ -339,6 +342,7 @@ class UnlockedDatabase(GObject.GObject):
             # For entry
             else:
                 self._stack.set_visible_child_name(self.current_element.uuid.urn)
+                self._linkedbox_right.hide()
                 self.entry_page.set_entry_page_headerbar()
 
     def add_page(self, scrolled_window: ScrolledPage, name: str) -> None:
@@ -375,6 +379,7 @@ class UnlockedDatabase(GObject.GObject):
         if group_page:
             self.set_browser_headerbar()
         else:
+            self._linkedbox_right.hide()
             self.entry_page.set_entry_page_headerbar()
 
     def _remove_page(self, element: Union[Entry, Group]) -> None:
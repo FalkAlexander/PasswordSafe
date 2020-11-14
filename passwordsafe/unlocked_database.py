FalkAlexander/PasswordSafe

commit fa67fe071bd49e1fb932371fd23d05e7d462fc28

unlocked_database: Regroup group_row click events
A click event on a GroupRow is handled by two diffents signals: the
"row-activated" signal from its container and the "button-press-event"
from the GtkEventBox it contains. There is no reason to have this
click logic split: all the cases can be handled by the
"button-press-event" event.

passwordsafe/unlocked_database.py
@@ -478,11 +478,6 @@ class UnlockedDatabase(GObject.GObject):
 
         if list_box_row.get_name() == "LoadMoreRow":
             self.search.on_load_more_row_clicked(list_box_row)
-            return
-        elif list_box_row.get_type() == "GroupRow":
-            self.current_element = self.database_manager.get_group(list_box_row.get_uuid())
-            self.pathbar.add_pathbar_button_to_pathbar(list_box_row.get_uuid())
-            self.show_page_of_new_directory(False, False)
 
     def on_save_button_clicked(self, widget):
         self.start_database_lock_timer()
@@ -598,15 +593,24 @@ class UnlockedDatabase(GObject.GObject):
         self.current_element = parent_group
         self.show_page_of_new_directory(False, False)
 
-    def on_group_row_button_pressed(self, widget, event):
+    def on_group_row_button_pressed(self, widget, event, group_row: GroupRow) -> bool:
         self.start_database_lock_timer()
-        if (event.type == Gdk.EventType.BUTTON_PRESS
-                and event.button == 3
+
+        if self.selection_ui.selection_mode_active:
+            self.selection_ui.row_selection_toggled(group_row)
+            return True
+
+        if (event.button == 3
                 and not self.props.search_active):
-            if self.selection_ui.selection_mode_active is False:
-                self.selection_ui.set_selection_headerbar(None, select_row=widget.get_parent())
-            else:
-                self.selection_ui.row_selection_toggled(widget.get_parent())
+            self.selection_ui.set_selection_headerbar(
+                None, select_row=group_row)
+        elif event.button == 1:
+            group_uuid = group_row.get_uuid()
+            self.current_element = self.database_manager.get_group(group_uuid)
+            self.pathbar.add_pathbar_button_to_pathbar(group_uuid)
+            self.show_page_of_new_directory(False, False)
+
+        return True
 
     def on_group_edit_button_clicked(self, button: Gtk.Button) -> None:
         """Edit button in a GroupRow was clicked

passwordsafe/group_row.py
@@ -27,7 +27,9 @@ class GroupRow(Gtk.ListBoxRow):
         builder.add_from_resource(
             "/org/gnome/PasswordSafe/unlocked_database.ui")
         group_event_box = builder.get_object("group_event_box")
-        group_event_box.connect("button-press-event", self.unlocked_database.on_group_row_button_pressed)
+        group_event_box.connect(
+            "button-press-event",
+            self.unlocked_database.on_group_row_button_pressed, self)
 
         group_name_label = builder.get_object("group_name_label")
 

commit 3132c6fa96e958fde12fd0898d8a12670c54eb83

unlocked_database: Fix group_row behavior on selection
While regrouping the group row events, a regression has been
introduced: group_row has now the same behavior as an entry. In
selection-mode, clicking on a group updates the selection-state
instead of opening the group. This change reverts group_row to its
normal behavior:

* not in selection-mode:
 - left click opens the group
 - right clicks enters selection-mode and selects the group

* in selection-mode:
 - left click opens the group
 - right click updates selection-state

Fixes: fa67fe071bd49e1fb932371fd23d05e7d462fc28

passwordsafe/unlocked_database.py
@@ -600,20 +600,20 @@ class UnlockedDatabase(GObject.GObject):
         # pylint: disable=too-many-arguments
         self.start_database_lock_timer()
 
-        if self.selection_ui.selection_mode_active:
-            self.selection_ui.row_selection_toggled(group_row)
-            return True
-
         button: int = gesture.get_current_button()
-        if (button == 3
-                and not self.props.search_active):
-            self.selection_ui.set_selection_headerbar(
-                None, select_row=group_row)
-        elif button == 1:
+        if button == 1:
             group_uuid = group_row.get_uuid()
             self.current_element = self.database_manager.get_group(group_uuid)
             self.pathbar.add_pathbar_button_to_pathbar(group_uuid)
             self.show_page_of_new_directory(False, False)
+            return True
+
+        if (button == 3
+                and not self.props.search_active):
+            if self.selection_ui.selection_mode_active:
+                self.selection_ui.row_selection_toggled(group_row)
+            else:
+                self.selection_ui.set_selection_headerbar(None, group_row)
 
         return True
 
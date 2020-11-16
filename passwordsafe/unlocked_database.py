FalkAlexander/PasswordSafe

commit 0cd0ddc48e71603188237ae96ba2357f53afc8f0

unlocked_database: Do reset the headerbar after an element deletion
Since, the rewrite of selection_mode, linkedbox_right is not removed
from the headerbar when entering selection-mode and re-added when
leaving selection-mode. Only its visibility is changed.

When an entry or a group is deleted, the parent group view is
completely repopulated by a call to swith_page. Then, the main
headerbar becomes visible by calling set_browser_headerbar. However,
in selection-mode, the headerbar does not need to be updated.

Fix the issue by not calling set_browser_headerbar in selection-mode.

passwordsafe/unlocked_database.py
@@ -376,7 +376,7 @@ class UnlockedDatabase(GObject.GObject):
         else:
             self._stack.set_visible_child_name(page_uuid.urn)
 
-        if group_page:
+        if group_page and not self.props.selection_mode:
             self.set_browser_headerbar()
         else:
             self._linkedbox_right.hide()

commit f8096f5f081fdb30492902865282e20d3d1ae081

unlocked_database: Do not show the secondary button in selection-mode
This is a follow up of commmit
0cd0ddc48e71603188237ae96ba2357f53afc8f0.
This secondary button only needs to be displayed if the current page
is an entry.

Fixes: 0cd0ddc48e71603188237ae96ba2357f53afc8f0
Closes: #249

passwordsafe/unlocked_database.py
@@ -381,7 +381,7 @@ class UnlockedDatabase(GObject.GObject):
 
         if group_page and not self.props.selection_mode:
             self.set_browser_headerbar()
-        else:
+        elif not group_page:
             self._linkedbox_right.hide()
             self.entry_page.set_entry_page_headerbar()
 
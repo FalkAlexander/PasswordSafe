81f70af5c96ac016e8fb951d0811c157a0e69d45
Maximiliano Sandoval R <msandova@protonmail.com>
2020-11-16 13:58:07 +0100

    Properties dialog: Make private
    Same treatment as references dialog. Make every attribute and method
    private. Properties dialog is called in precisely one point in the code
    outside of properties_dialog.py.

--- passwordsafe/properties_dialog.py
+++ passwordsafe/properties_dialog.py
@@ -9,43 +9,47 @@ class PropertiesDialog:
     """Displays a modal dialog with Entry/Group Properties"""
 
     def __init__(self, database):
-        self.builder = Gtk.Builder()
-        self.builder.add_from_resource("/org/gnome/PasswordSafe/properties_dialog.ui")
-        self.dialog = self.builder.get_object("properties_dialog")
-        self._database = database
-        self._db_manager = database.database_manager
-        self.dialog.set_transient_for(self._database.window)
-        self.update_properties()
-        self.connect_signals()
+        self.__builder = Gtk.Builder()
+        self.__builder.add_from_resource("/org/gnome/PasswordSafe/properties_dialog.ui")
+        self.__dialog = self.__builder.get_object("properties_dialog")
+        self.__database = database
+        self.__db_manager = database.database_manager
+        self.__setup_signals()
+        self.__setup_widgets()
 
     def present(self):
         """Present the dialog"""
-        self.dialog.present()
+        self.__dialog.present()
 
-    def update_properties(self) -> None:
+    def __update_properties(self) -> None:
         """Construct dialog content with the attributes of the Entry|Group"""
-        element = self._database.current_element
+        element = self.__database.current_element
         hex_uuid = element.uuid.hex.upper()
-        self.builder.get_object("label_uuid").set_text(hex_uuid)
-        self.builder.get_object("label_accessed").set_text(
-            self._db_manager.get_element_acessed_date(element)
+        self.__builder.get_object("label_uuid").set_text(hex_uuid)
+        self.__builder.get_object("label_accessed").set_text(
+            self.__db_manager.get_element_acessed_date(element)
         )
-        self.builder.get_object("label_modified").set_text(
-            self._db_manager.get_element_modified_date(element)
+        self.__builder.get_object("label_modified").set_text(
+            self.__db_manager.get_element_modified_date(element)
         )
-        self.builder.get_object("label_created").set_text(
-            self._db_manager.get_element_creation_date(element)
+        self.__builder.get_object("label_created").set_text(
+            self.__db_manager.get_element_creation_date(element)
         )
 
-    def connect_signals(self) -> None:
-        self.dialog.connect("key-press-event", self._on_key_press_event)
-        self._db_manager.connect("notify::locked", self.__on_locked)
+    def __setup_signals(self) -> None:
+        self.__dialog.connect("key-press-event", self.__on_key_press_event)
+        self.__db_manager.connect("notify::locked", self.__on_locked)
 
-    def _on_key_press_event(self, _window: Handy.Window, event: Gtk.Event) -> bool:
+    def __setup_widgets(self) -> None:
+        self.__update_properties()
+        self.__dialog.set_modal(True)
+        self.__dialog.set_transient_for(self.__database.window)
+
+    def __on_key_press_event(self, _window: Handy.Window, event: Gtk.Event) -> bool:
         if event.keyval == Gdk.KEY_Escape:
-            self.dialog.close()
+            self.__dialog.close()
             return Gdk.EVENT_STOP
         return Gdk.EVENT_PROPAGATE
 
     def __on_locked(self, _database_manager, _value):
-        self.dialog.close()
+        self.__dialog.close()

0498d8b724fb1c0b1a91d51fc8045e2ec2417ad4
Maximiliano Sandoval R <msandova@protonmail.com>
2020-11-22 23:03:45 +0100

    Remove xalign

--- passwordsafe/error_info_bar.py
+++ passwordsafe/error_info_bar.py
@@ -20,14 +20,12 @@ class ErrorInfoBar(Gtk.InfoBar):
 
         main_label = Gtk.Label(label=main_text)
         main_label.get_style_context().add_class("main")
-        main_label.props.xalign = 0.
         main_label.props.ellipsize = Pango.EllipsizeMode.END
         # main_label.props.max_width_chars = 50
         content.add(main_label)
 
         secondary_label = Gtk.Label(label=secondary_text)
         secondary_label.get_style_context().add_class("secondary")
-        secondary_label.props.xalign = 0.
         secondary_label.props.ellipsize = Pango.EllipsizeMode.END
         content.add(secondary_label)
 

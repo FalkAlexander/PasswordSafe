9a30fb2282d53a02d5fc1fa25c54f47814a512de
Jean Felder <jfelder@src.gnome.org>
2020-11-25 15:55:04 +0100

    color_button: Directly inherit from a flowboxchild
    When a widget is added is inserted into a FlowBox, a FlowBoxChild is
    inserted if the widget is not a FlowBoxChild itself. By direclty
    inheriting from a FlowBoxChild, it simplifies the code and allows to
    use the "child-activated" signal from the FlowBox. The css also needs
    to be adapted to have the same circular effect.

--- passwordsafe/color_widget.py
+++ passwordsafe/color_widget.py
@@ -23,7 +23,7 @@ class Color(Enum):
 
 
 @Gtk.Template(resource_path="/org/gnome/PasswordSafe/color_button.ui")
-class ColorButton(Gtk.RadioButton):  # pylint: disable=too-few-public-methods
+class ColorButton(Gtk.FlowBoxChild):  # pylint: disable=too-few-public-methods
 
     __gtype_name__ = "ColorButton"
 
@@ -87,19 +87,18 @@ class ColorEntryRow(Gtk.ListBoxRow):  # pylint: disable=too-few-public-methods
         for color in Color:
             active: bool = (self._selected_color == color.value)
             color_button: ColorButton = ColorButton(color, active)
-            color_button.connect("clicked", self._on_color_activated)
             self._flowbox.insert(color_button, -1)
 
-    def _on_color_activated(self, selected_button: Gtk.Button) -> None:
+    @Gtk.Template.Callback()
+    def _on_color_activated(
+            self, _flowbox: Gtk.FlowBox, selected_child: Gtk.FlowBoxChild) -> None:
         self._unlocked_database.start_database_lock_timer()
-        if selected_button.props.selected:
+        if selected_child.props.selected:
             return
 
         for child in self._flowbox.get_children():
-            color_button = child.get_children()[0]
-            selected: bool = (color_button == selected_button)
-            color_button.props.selected = selected
+            selected: bool = (child == selected_child)
+            child.props.selected = selected
 
         self._scrolled_page.is_dirty = True
-        self._db_manager.set_entry_color(
-            self._entry_uuid, selected_button.color)
+        self._db_manager.set_entry_color(self._entry_uuid, selected_child.color)

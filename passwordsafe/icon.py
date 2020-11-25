40fc4f809939524f21b5037a3d8b238a23d80d0d
Jean Felder <jfelder@src.gnome.org>
2020-11-25 13:50:34 +0100

    icon: Remove unused icons
    They are not exposed in the EntryPage. They cannot be handled
    correctly. They will be added again in a following commit once
    SafeEntry has been updated to properly handle them.

--- passwordsafe/icon.py
+++ passwordsafe/icon.py
@@ -1,77 +1,18 @@
 # SPDX-License-Identifier: GPL-3.0-only
 from typing import Optional
 
-icon_list = [
-    "dialog-password-symbolic",  # 00
-    "network-wired-symbolic",  # 01
-    "dialog-warning-symbolic",  # 02
-    "network-server-symbolic",
-    "document-edit-symbolic",
-    "media-view-subtitles-symbolic",
-    "application-x-addon-symbolic",
-    "accessories-text-editor-symbolic",
-    "network-wired-symbolic",
-    "mail-send-symbolic",
-    "text-x-generic-symbolic",  # 10
-    "camera-photo-symbolic",
-    "network-wireless-signal-excellent-symbolic",
-    "dialog-password-symbolic",
-    "colorimeter-colorhug-symbolic",
-    "scanner-symbolic",
-    "network-wired-symbolic",
-    "media-optical-cd-audio-symbolic",
-    "video-display-symbolic",
-    "mail-unread-symbolic",
-    "emblem-system-symbolic",  # 20
-    "edit-paste-symbolic",
-    "edit-paste-symbolic",
-    "preferences-desktop-remote-desktop-symbolic",
-    "uninterruptible-power-supply-symbolic",
-    "mail-unread-symbolic",
-    "media-floppy-symbolic",
-    "drive-harddisk-symbolic",
-    "dialog-password-symbolic",
-    "dialog-password-symbolic",
-    "utilities-terminal-symbolic",  # 30
-    "printer-symbolic",
-    "image-x-generic-symbolic",
-    "edit-select-all-symbolic",
-    "preferences-system-symbolic",
-    "network-workgroup-symbolic",
-    "dialog-password-symbolic",
-    "dialog-password-symbolic",
-    "drive-harddisk-symbolic",
-    "document-open-recent-symbolic",
-    "system-search-symbolic",  # 40
-    "dialog-password-symbolic",
-    "media-flash-symbolic",
-    "user-trash-symbolic",
-    "accessories-text-editor-symbolic",
-    "edit-delete-symbolic",
-    "dialog-question-symbolic",
-    "package-x-generic-symbolic",
-    "folder-symbolic",
-    "folder-open-symbolic",
-    "document-open-symbolic",  # 50
-    "system-lock-screen-symbolic",
-    "rotation-locked-symbolic",
-    "object-select-symbolic",
-    "document-edit-symbolic",
-    "image-x-generic-symbolic",
-    "accessories-dictionary-symbolic",
-    "view-list-symbolic",
-    "avatar-default-symbolic",
-    "applications-engineering-symbolic",
-    "go-home-symbolic",  # 60
-    "starred-symbolic",  # 61
-    "start-here-symbolic",  # 62
-    "dialog-password-symbolic",  # 63
-    "start-here-symbolic",  # 64
-    "accessories-dictionary-symbolic",  # 65
-    "dialog-password-symbolic",  # 66
-    "application-certificate-symbolic",  # 67
-    "phone-apple-iphone-symbolic",  # 68
-]
+icon_list = {
+    "0": "dialog-password-symbolic",
+    "1": "network-wired-symbolic",
+    "9": "mail-send-symbolic",
+    "12": "network-wireless-signal-excellent-symbolic",
+    "19": "mail-unread-symbolic",
+    "23": "preferences-desktop-remote-desktop-symbolic",
+    "27": "drive-harddisk-symbolic",
+    "30": "utilities-terminal-symbolic",
+    "34": "preferences-system-symbolic",
+    "48": "folder-symbolic"
+}
 
 
 def get_icon_name(number: Optional[int]) -> str:
@@ -81,6 +22,7 @@ def get_icon_name(number: Optional[int]) -> str:
     # use default icon if number is None or out of range
     if number is None:
         number = 0
-    if number < 0 or number > 68:
-        number = 0
-    return icon_list[number]
+    try:
+        return icon_list[str(number)]
+    except KeyError:
+        return icon_list["0"]

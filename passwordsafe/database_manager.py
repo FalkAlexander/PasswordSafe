FalkAlexander/PasswordSafe

commit e0a669af00f515cfd54e92af3856e8a78d0b4ee2

database_manager: Return a list of group and entry for search

passwordsafe/database_manager.py
@@ -636,10 +636,10 @@ class DatabaseManager(GObject.GObject):
         return True
 
     # Search for an entry or a group
-    def search(self, string: str, path: str) -> List[UUID]:
+    def search(self, string: str, path: str) -> List[Union[Entry, Group]]:
         full_text_search = passwordsafe.config_manager.get_full_text_search()
         global_search = not passwordsafe.config_manager.get_local_search()
-        uuid_list = []
+        results: List[Union[Entry, Group]] = []
 
         def search_entry(entry: Union[Entry, Group], lookfor: List[Union[None, str]]) -> None:
             """Looks if the querry string appears in any on the items of lookfor.
@@ -649,12 +649,12 @@ class DatabaseManager(GObject.GObject):
                 if term is None:
                     continue
                 if string.lower() in term.lower():
-                    if global_search and entry.uuid not in uuid_list:
-                        uuid_list.append(entry.uuid)
+                    if global_search and entry not in results:
+                        results.append(entry.uuid)
                     elif self.get_parent_group(entry) is not None:
                         parent_group: Group = self.get_parent_group(entry)
-                        if parent_group.path == path and entry.uuid not in uuid_list:
-                            uuid_list.append(entry.uuid)
+                        if parent_group.path == path and entry not in results:
+                            results.append(entry)
 
         for group in self.db.groups:
             if group.is_root_group is False:
@@ -671,7 +671,7 @@ class DatabaseManager(GObject.GObject):
                 url = entry.url
                 search_entry(entry, [username, notes, url])
 
-        return uuid_list
+        return results
 
     def check_is_group(self, uuid):
         """Whether uuid is a group uuid"""

passwordsafe/search.py
@@ -4,8 +4,7 @@ from __future__ import annotations
 import threading
 import typing
 
-from typing import List
-from uuid import UUID
+from typing import List, Union
 from gi.repository import Gdk, GLib, GObject, Gtk, Handy
 
 from passwordsafe.entry_row import EntryRow
@@ -13,6 +12,9 @@ from passwordsafe.group_row import GroupRow
 from passwordsafe.scrolled_page import ScrolledPage
 
 if typing.TYPE_CHECKING:
+    from pykeepass.entry import Entry
+    from pykeepass.group import Group
+
     from passwordsafe.database_manager import DatabaseManager
     from passwordsafe.unlocked_database import UnlockedDatabase
 
@@ -54,7 +56,7 @@ class Search:
         self._timeout_search: int = 0
 
         self._timeout_info: int = 0
-        self._result_list: List[UUID] = []
+        self._result_list: List[Union[Entry, Group]] = []
         self._search_text: str = self._search_entry.props.text
         self._current_result_idx: int = 0
 
@@ -193,18 +195,15 @@ class Search:
         skipped_rows: bool = False
 
         new_idx: int = 0
-        for uuid in self._result_list[self._current_result_idx:]:
+        for element in self._result_list[self._current_result_idx:]:
             if search_height < window_height or load_all:
-                if self._db_manager.check_is_group(uuid):
+                if self._db_manager.check_is_group_object(element):
                     search_height += group_row_height
                     row = GroupRow(
-                        self.unlocked_database, self._db_manager,
-                        self._db_manager.get_group(uuid))
+                        self.unlocked_database, self._db_manager, element)
                 else:
                     search_height += entry_row_height
-                    row = EntryRow(
-                        self.unlocked_database,
-                        self._db_manager.get_entry_object_from_uuid(uuid))
+                    row = EntryRow(self.unlocked_database, element)
 
                 self.search_list_box.add(row)
                 new_idx += 1

commit b918840e1143388186b8a315ec84018e94975be8

database_manager: In search, only add entry or group not uuid
Since commit e0a669af00f515cfd54e92af3856e8a78d0b4ee2, results is only
supposed to handle Entry or Group, not UUID. However, one case was
forgotten. This fixes it.

Fixes: commit e0a669af00f515cfd54e92af3856e8a78d0b4ee2

passwordsafe/database_manager.py
@@ -650,7 +650,7 @@ class DatabaseManager(GObject.GObject):
                     continue
                 if string.lower() in term.lower():
                     if global_search and entry not in results:
-                        results.append(entry.uuid)
+                        results.append(entry)
                     elif self.get_parent_group(entry) is not None:
                         parent_group: Group = self.get_parent_group(entry)
                         if parent_group.path == path and entry not in results:
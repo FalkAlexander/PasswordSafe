a342ba9399eda2ee61c7f681133983d3866a8165
Maximiliano Sandoval R <msandova@protonmail.com>
2020-12-18 19:27:19 +0100

    Entry Page: Port save filechooser

--- passwordsafe/entry_page.py
+++ passwordsafe/entry_page.py
@@ -15,6 +15,7 @@ from passwordsafe.password_entry_row import PasswordEntryRow
 
 if typing.TYPE_CHECKING:
     from pykeepass.entry import Entry
+    from pykeepass.attachment import Attachment
 
 
 class EntryPage:
@@ -22,6 +23,7 @@ class EntryPage:
     #
     # Global Variables
     #
+    _filechooser = None
     unlocked_database = NotImplemented
     _pwd_popover = NotImplemented
 
@@ -533,20 +535,26 @@ class EntryPage:
         AttachmentWarningDialog(self, attachment).present()
 
     def on_attachment_download_button_clicked(self, _button, attachment):
+        print(attachment)
         save_dialog = Gtk.FileChooserNative.new(
             # NOTE: Filechooser title for downloading an attachment
             _("Save attachment"), self.unlocked_database.window, Gtk.FileChooserAction.SAVE,
             _("Save"), None)
-        save_dialog.set_do_overwrite_confirmation(True)
         save_dialog.set_current_name(attachment.filename)
         save_dialog.set_modal(True)
-        save_dialog.set_local_only(False)
 
-        response = save_dialog.run()
+        self._filechooser = save_dialog
+        save_dialog.connect("response", self._on_save_filechooser_response, attachment)
+        save_dialog.show()
 
+    def _on_save_filechooser_response(self,
+                                      dialog: Gtk.Dialog,
+                                      response: Gtk.ResponseType,
+                                      attachment: Attachment) -> None:
+        self._filechooser = None
         if response == Gtk.ResponseType.ACCEPT:
             bytes_buffer = self.unlocked_database.database_manager.db.binaries[attachment.id]
-            self.save_to_disk(save_dialog.get_filename(), bytes_buffer)
+            self.save_to_disk(dialog.get_file().get_path(), bytes_buffer)
 
     def on_attachment_delete_button_clicked(self, _button, attachment, attachment_row):
         entry = self.unlocked_database.current_element

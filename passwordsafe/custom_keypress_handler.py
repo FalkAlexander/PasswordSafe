4187fed1c6bfc784838658a428519bcbceb9b99f
Maximiliano Sandoval R <msandova@protonmail.com>
2020-12-22 19:11:09 +0100

    Custom Keypress Handler: Remove tab
    This was a really buggy way of handling TAB. It was buggy and did not
    work, with the recent changes TAB works as intended now.
    
    Fixes: https://gitlab.gnome.org/World/PasswordSafe/-/issues/182

--- passwordsafe/custom_keypress_handler.py
+++ passwordsafe/custom_keypress_handler.py
@@ -40,16 +40,12 @@ class CustomKeypressHandler:
             return Gdk.EVENT_PROPAGATE
 
         scrolled_page = self.unlocked_database.get_current_page()
-        if (scrolled_page.edit_page
-                and eventkey.keyval == Gdk.KEY_Tab):
-            focused_entry = self.unlocked_database.window.get_focus()
-            if focused_entry and "TabBox" in focused_entry.get_name():
-                self.tab_to_next_input_entry(scrolled_page)
-                return Gdk.EVENT_PROPAGATE
 
         # Handle undo and redo on entries.
-        elif (scrolled_page.edit_page
-              and eventkey.state & Gdk.ModifierType.CONTROL_MASK == Gdk.ModifierType.CONTROL_MASK):
+        if (
+                scrolled_page.edit_page
+                and eventkey.state & Gdk.ModifierType.CONTROL_MASK == Gdk.ModifierType.CONTROL_MASK
+        ):
             keyval_name = Gdk.keyval_name(eventkey.keyval)
             if isinstance(window.get_focus(), Gtk.TextView):
                 textbuffer = window.get_focus().get_buffer()
@@ -76,52 +72,6 @@ class CustomKeypressHandler:
             self.unlocked_database.props.search_active = True
 
         return Gdk.EVENT_PROPAGATE
-
-    def tab_to_next_input_entry(self, scrolled_page):
-        focus_widget = self.unlocked_database.window.get_focus()
-        focus_widget_index = focus_widget.get_parent().get_children().index(focus_widget)
-        new_index = focus_widget_index + 1
-        if new_index < len(focus_widget.get_parent().get_children()):
-            if focus_widget.get_parent().get_children()[new_index].get_name() == "TabBox_Next":
-                self.unlocked_database.window.set_focus(focus_widget.get_parent().get_children()[new_index])
-                return
-
-        rows = scrolled_page.properties_list_box.get_children()
-        current_row = self.iterate_parents(self.unlocked_database.window.get_focus())
-        current_index = rows.index(current_row)
-        new_index = current_index + 1
-        if new_index < len(rows):
-            next_row = rows[new_index]
-        else:
-            next_row = rows[0]
-
-        if next_row.get_name() == "ShowAllRow":
-            next_row = rows[0]
-
-        self.interate_to_next_input(next_row)
-
-    def interate_to_next_input(self, parent):
-        if parent.get_name() == "TabBox":
-            self.unlocked_database.window.set_focus(parent)
-            return
-
-        if hasattr(parent, "get_children"):
-            for child in parent.get_children():
-                if child.get_name() == "TabBox":
-                    self.unlocked_database.window.set_focus(child)
-                else:
-                    self.interate_to_next_input(child)
-
-    def iterate_parents(self, child):
-        """Return `child` or the first parent of it which is a Gtk.ListBoxRow
-
-        :returns: `child` or the first parent of it which is a Gtk.ListBoxRow
-                  or `None` if nothing matches."""
-        if isinstance(child, Gtk.ListBoxRow):
-            return child
-        if hasattr(child, "get_parent"):
-            return self.iterate_parents(child.get_parent())
-        return None
 
     def _current_view_accessible(self):
         """Check that the current view is accessible:

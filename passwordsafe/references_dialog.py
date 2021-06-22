6cc9a8dbb06482d5e1751d842459e1a54d0610ae
Maximiliano Sandoval R <msandova@protonmail.com>
2021-06-22 21:15:34 +0200

    references_dialog: Port to Template

--- passwordsafe/references_dialog.py
+++ passwordsafe/references_dialog.py
@@ -1,39 +1,39 @@
 # SPDX-License-Identifier: GPL-3.0-only
 from __future__ import annotations
 
-from typing import Any
-
-from gi.repository import Gio, Gtk
+from gi.repository import Adw, Gio, GLib, Gtk
 
 
-class ReferencesDialog:
+@Gtk.Template(resource_path="/org/gnome/PasswordSafe/gtk/references_dialog.ui")
+class ReferencesDialog(Adw.Window):
+
+    __gtype_name__ = "ReferencesDialog"
+
     __property = "P"
 
+    _codes_popover = Gtk.Template.Child()
+    _property_label = Gtk.Template.Child()
+    _property_popover_button = Gtk.Template.Child()
+    _reference_entry = Gtk.Template.Child()
+    _uuid_label = Gtk.Template.Child()
+    _uuid_popover = Gtk.Template.Child()
+
     def __init__(self, unlocked_database):
+        super().__init__()
+
         self.__unlocked_database = unlocked_database
         self.__database_manager = unlocked_database.database_manager
-        self.__builder = Gtk.Builder()
-        self.__builder.add_from_resource("/org/gnome/PasswordSafe/references_dialog.ui")
-
-        self.__reference_entry = self.__builder.get_object("reference_entry")
-        self.__dialog = self.__builder.get_object("references_dialog")
 
         self.__setup_actions()
         self.__setup_signals()
         self.__setup_widgets()
 
-    def present(self) -> None:
-        self.__dialog.present()
-
     def __setup_widgets(self) -> None:
-        self.__dialog.set_modal(True)
-        self.__dialog.set_transient_for(self.__unlocked_database.window)
+        self.set_modal(True)
+        self.set_transient_for(self.__unlocked_database.window)
         self.__update_reference_entry()
 
     def __setup_signals(self) -> None:
-        self.__reference_entry.connect(
-            "icon-press", self.__on_copy_secondary_button_clicked
-        )
         self.__connect_model_buttons_signals()
         self.__database_manager.connect("notify::locked", self.__on_locked)
 
@@ -43,50 +43,38 @@ class ReferencesDialog:
             simple_action = Gio.SimpleAction.new(label, None)
             simple_action.connect("activate", self.__on_property_model_button_clicked)
             action_group.insert(simple_action)
-        self.__dialog.insert_action_group("reference", action_group)
+        self.insert_action_group("reference", action_group)
 
     def __connect_model_buttons_signals(self):
-        self.__builder.get_object("property_label_gesture").connect(
-            "pressed", self.__open_codes_popover
-        )
-        self.__builder.get_object("identifier_label_gesture").connect(
-            "pressed", self.__open_codes_popover
-        )
-        self.__builder.get_object("uuid_label_gesture").connect(
-            "pressed", self.__open_uuid_popover
-        )
-
-        uuid_popover = self.__builder.get_object("uuid_popover")
-        uuid_popover.set_parent(self.__builder.get_object("uuid_label"))
-
-        codes_popover = self.__builder.get_object("codes_popover")
-        codes_popover.set_parent(self.__builder.get_object("property_label"))
+        self._uuid_popover.set_parent(self._uuid_label)
+        self._codes_popover.set_parent(self._property_label)
 
     def __update_reference_entry(self):
         """Update the reference entry and selected label text."""
         uuid = self.__unlocked_database.current_element.uuid
         encoded_uuid = uuid.hex.upper()
 
-        self.__builder.get_object("property_popover_button").set_label(self.__property)
+        self._property_popover_button.set_label(self.__property)
 
-        self.__reference_entry.set_text(
+        self._reference_entry.set_text(
             "{REF:" + self.__property + "@I:" + encoded_uuid + "}"
         )
 
-    def __open_codes_popover(self, _gesture, _n_points, _x, _y, _data=None):
-        codes_popover = self.__builder.get_object("codes_popover")
-        codes_popover.popup()
+    @Gtk.Template.Callback()
+    def _open_codes_popover(self, _gesture, _n_points, _x, _y):
+        self._codes_popover.popup()
 
-    def __open_uuid_popover(self, _gesture, _n_points, _x, _y, _data=None):
-        uuid_popover = self.__builder.get_object("uuid_popover")
-        uuid_popover.popup()
+    @Gtk.Template.Callback()
+    def _open_uuid_popover(self, _gesture, _n_points, _x, _y):
+        self._uuid_popover.popup()
 
-    def __on_copy_secondary_button_clicked(self, entry, _position):
+    @Gtk.Template.Callback()
+    def _on_copy_secondary_button_clicked(self, entry, _position):
         self.__unlocked_database.start_database_lock_timer()
         self.__unlocked_database.clipboard.set(entry.get_text())
 
     def __on_property_model_button_clicked(
-        self, action: Gio.SimpleAction, _data: Any = None
+        self, action: Gio.Action, _parameter: GLib.Variant
     ) -> None:
         self.__unlocked_database.start_database_lock_timer()
         self.__property = action.props.name
@@ -95,4 +83,4 @@ class ReferencesDialog:
     def __on_locked(self, database_manager, _value):
         locked = database_manager.props.locked
         if locked:
-            self.__dialog.close()
+            self.close()

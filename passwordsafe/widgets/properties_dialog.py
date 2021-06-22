a20d72b1a59e0e4cdc44084486dfd1ecff060892
Maximiliano Sandoval R <msandova@protonmail.com>
2021-06-22 21:03:20 +0200

    properties_dialog: Port to Template

--- passwordsafe/widgets/properties_dialog.py
+++ passwordsafe/widgets/properties_dialog.py
@@ -2,37 +2,40 @@
 """Responsible for displaying the Entry/Group Properties"""
 from __future__ import annotations
 
-from gi.repository import Gtk
+from gi.repository import Adw, Gtk
 
 
-class PropertiesDialog:
+@Gtk.Template(resource_path="/org/gnome/PasswordSafe/gtk/properties_dialog.ui")
+class PropertiesDialog(Adw.Window):
     """Displays a modal dialog with Entry/Group Properties"""
 
+    __gtype_name__ = "PropertiesDialog"
+
+    _accessed_label = Gtk.Template.Child()
+    _created_label = Gtk.Template.Child()
+    _modified_label = Gtk.Template.Child()
+    _uuid_label = Gtk.Template.Child()
+
     def __init__(self, database):
-        self.__builder = Gtk.Builder()
-        self.__builder.add_from_resource("/org/gnome/PasswordSafe/properties_dialog.ui")
-        self.__dialog = self.__builder.get_object("properties_dialog")
+        super().__init__()
+
         self.__database = database
         self.__db_manager = database.database_manager
         self.__setup_signals()
         self.__setup_widgets()
 
-    def present(self):
-        """Present the dialog"""
-        self.__dialog.present()
-
     def __update_properties(self) -> None:
         """Construct dialog content with the attributes of the Entry|Group"""
         element = self.__database.current_element
         hex_uuid = element.uuid.hex.upper()
-        self.__builder.get_object("label_uuid").set_text(hex_uuid)
-        self.__builder.get_object("label_accessed").set_text(
+        self._uuid_label.set_text(hex_uuid)
+        self._accessed_label.set_text(
             self.__db_manager.get_element_acessed_date(element)
         )
-        self.__builder.get_object("label_modified").set_text(
+        self._modified_label.set_text(
             self.__db_manager.get_element_modified_date(element)
         )
-        self.__builder.get_object("label_created").set_text(
+        self._created_label.set_text(
             self.__db_manager.get_element_creation_date(element)
         )
 
@@ -41,10 +44,10 @@ class PropertiesDialog:
 
     def __setup_widgets(self) -> None:
         self.__update_properties()
-        self.__dialog.set_modal(True)
-        self.__dialog.set_transient_for(self.__database.window)
+        self.set_modal(True)
+        self.set_transient_for(self.__database.window)
 
     def __on_locked(self, database_manager, _value):
         locked = database_manager.props.locked
         if locked:
-            self.__dialog.close()
+            self.close()

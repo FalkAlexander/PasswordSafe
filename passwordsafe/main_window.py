06bf78eab09008869410f7faac22c846811e0efe
Maximiliano Sandoval R <msandova@protonmail.com>
2021-06-24 10:12:18 +0200

    window: Correctly invoke recent files headerbar
    This was not crashing the app, nor producing an incorrect headerbar, but
    it showed an error.

--- passwordsafe/main_window.py
+++ passwordsafe/main_window.py
@@ -180,7 +180,7 @@ class MainWindow(Adw.ApplicationWindow):
     def display_welcome_page(self) -> None:
         """Shown when there is no autoloading and no recent files to display"""
         self._main_view.set_visible_child(self.welcome_page)
-        self._headerbar_stack.set_visible_child_name("recent_files_headerbar")
+        self._headerbar_stack.set_visible_child(self._recent_files_headerbar)
 
     #
     # Open Database Methods

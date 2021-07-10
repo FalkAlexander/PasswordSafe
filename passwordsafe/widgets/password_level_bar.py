8fc734aa3177aadcdebc4b69c5f60103e28fb8c2
Maximiliano Sandoval R <msandova@protonmail.com>
2021-07-10 11:32:43 +0200

    password_level: Bind property
    Rather than setting an entry. This is way more direct.

--- passwordsafe/widgets/password_level_bar.py
+++ passwordsafe/widgets/password_level_bar.py
@@ -10,7 +10,6 @@ class PasswordLevelBar(Gtk.LevelBar):
 
     __gtype_name__ = "PasswordLevelBar"
 
-    _entry: Gtk.Editable | None = None
     _password: str = ""
 
     def __init__(self) -> None:
@@ -26,15 +25,6 @@ class PasswordLevelBar(Gtk.LevelBar):
         self.add_offset_value("medium", 3.49)
         self.add_offset_value("strong", 4.49)
         self.add_offset_value("secure", 5.0)
-
-    @GObject.Property(type=Gtk.Editable)
-    def entry(self) -> Gtk.Editable:
-        return self._entry
-
-    @entry.setter  # type: ignore
-    def entry(self, entry: Gtk.Editable) -> None:
-        self._entry = entry
-        entry.bind_property("text", self, "password", GObject.BindingFlags.SYNC_CREATE)
 
     @GObject.Property(type=str, default="")
     def password(self) -> str:

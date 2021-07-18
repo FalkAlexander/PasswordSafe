80b3e3b2c1e754702d068e60f2ec94d884fd3c51
Maximiliano Sandoval R <msandova@protonmail.com>
2021-07-18 09:23:21 +0200

    sorting: Separate ctime ordering into asc and dec

--- passwordsafe/sorting.py
+++ passwordsafe/sorting.py
@@ -17,7 +17,8 @@ class SortingHat:
     class SortOrder(IntEnum):
         ASC = 0
         DEC = 1
-        CTIME_DEC = 2
+        CTIME_ASC = 2
+        CTIME_DEC = 3
 
     # will be set from just below the class
     sort_funcs: typing.Dict[SortOrder, typing.Callable] = {}
@@ -47,7 +48,7 @@ class SortingHat:
         return GLib.ascii_strcasecmp(ele2.name, ele1.name)
 
     @staticmethod
-    def sort_by_ctime_dec(ele1: SafeElement, ele2: SafeElement) -> int:
+    def sort_by_ctime_asc(ele1: SafeElement, ele2: SafeElement) -> int:
         # <0 if ele1<ele2, 0 if ele1=ele2, >0 ele1>ele2
         if ele1.ctime is None or ele2.ctime is None:
             return 0
@@ -58,9 +59,22 @@ class SortingHat:
 
         return ele1.ctime.compare(ele2.ctime)
 
+    @staticmethod
+    def sort_by_ctime_dec(ele1: SafeElement, ele2: SafeElement) -> int:
+        # <0 if ele1<ele2, 0 if ele1=ele2, >0 ele1>ele2
+        if ele1.ctime is None or ele2.ctime is None:
+            return 0
+        if ele1.is_group and ele2.is_entry:
+            return -1
+        if ele1.is_entry and ele2.is_group:
+            return 1
+
+        return ele2.ctime.compare(ele1.ctime)
+
 
 SortingHat.sort_funcs = {
     SortingHat.SortOrder.ASC: SortingHat.sort_by_name_asc,
     SortingHat.SortOrder.DEC: SortingHat.sort_by_name_dec,
+    SortingHat.SortOrder.CTIME_ASC: SortingHat.sort_by_ctime_asc,
     SortingHat.SortOrder.CTIME_DEC: SortingHat.sort_by_ctime_dec,
 }

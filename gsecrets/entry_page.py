1c7956639350db8dad28ce532d57741a40752aaa
Maximiliano Sandoval R <msandova@protonmail.com>
2022-07-18 12:11:10 +0200

    entry_page: Port otp to AdwEntryRow

--- gsecrets/entry_page.py
+++ gsecrets/entry_page.py
@@ -36,13 +36,10 @@ class EntryPage(Gtk.Box):
 
     password_entry_row = Gtk.Template.Child()
 
-    otp_token_box = Gtk.Template.Child()
-    otp_token_row = Gtk.Template.Child()
-    otp_progress_icon = Gtk.Template.Child()
-
     otp_error_revealer = Gtk.Template.Child()
-    otp_property_box = Gtk.Template.Child()
-    otp_property_value_entry = Gtk.Template.Child()
+    otp_progress_icon = Gtk.Template.Child()
+    otp_secret_entry_row = Gtk.Template.Child()
+    otp_token_row = Gtk.Template.Child()
 
     notes_property_box = Gtk.Template.Child()
     notes_property_value_entry = Gtk.Template.Child()
@@ -78,7 +75,6 @@ class EntryPage(Gtk.Box):
 
         self.unlocked_database = u_d
         self.toggeable_widget_list = [
-            self.otp_property_box,
             self.notes_property_box,
             self.attachment_property_box,
             self.attributes_property_box,
@@ -137,7 +133,6 @@ class EntryPage(Gtk.Box):
         safe_entry.connect("notify::otp", self.otp_update)
         if safe_entry.otp_token():
             self.otp_update(safe_entry, None)
-            self.show_row(self.otp_token_box, True, add_all)
 
         # Url
         safe_entry.bind_property(
@@ -150,11 +145,10 @@ class EntryPage(Gtk.Box):
         # OTP (secret)
         safe_entry.bind_property(
             "otp",
-            self.otp_property_value_entry,
+            self.otp_secret_entry_row,
             "text",
             GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
         )
-        self.show_row(self.otp_property_box, False, add_all)
 
         # Notes
         textbuffer = self.notes_property_value_entry.get_buffer()
@@ -408,15 +402,21 @@ class EntryPage(Gtk.Box):
         if otp_token:
             remaining_time = safe_entry.otp_lifespan() / safe_entry.otp_interval()
             self.otp_progress_icon.props.progress = remaining_time
-            self.otp_token_box.props.visible = True
+            self.otp_token_row.show()
             self.otp_token_row.props.title = otp_token
             self.otp_timer_handler = GObject.timeout_add(
                 100, self.otp_update, safe_entry, None
             )
         else:
-            self.otp_token_box.props.visible = False
+            self.otp_token_row.hide()
 
-        reveal_error = not otp_token and safe_entry.props.otp
-        self.otp_error_revealer.reveal(reveal_error)
+        if not otp_token and safe_entry.props.otp:
+            self.otp_error_revealer.reveal(True)
+            self.otp_token_row.hide()
+            self.otp_secret_entry_row.add_css_class("error")
+        else:
+            self.otp_error_revealer.reveal(False)
+            self.otp_token_row.show()
+            self.otp_secret_entry_row.remove_css_class("error")
 
         return GLib.SOURCE_CONTINUE

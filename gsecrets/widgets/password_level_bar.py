a7f3590c9d2f9636049d28cfd8e15af22a0bfa49
Maximiliano Sandoval R <msandova@gnome.org>
2022-11-23 19:23:45 +0100

    pw_level_bar: Do not subclass GtkLevelBar
    It is a final class.

--- gsecrets/widgets/password_level_bar.py
+++ gsecrets/widgets/password_level_bar.py
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-3.0-only
 from __future__ import annotations
 
-from gi.repository import GObject, Gtk
+from gi.repository import Adw, GObject, Gtk
 
 from gsecrets.password_generator import strength_async
 
 
-class PasswordLevelBar(Gtk.LevelBar):
+class PasswordLevelBar(Adw.Bin):
 
     __gtype_name__ = "PasswordLevelBar"
 
@@ -15,16 +15,20 @@ class PasswordLevelBar(Gtk.LevelBar):
     def __init__(self) -> None:
         super().__init__()
 
-        self.props.max_value = 4.0
-        self.props.mode = Gtk.LevelBarMode.DISCRETE
+        self.level_bar = Gtk.LevelBar()
+
+        self.level_bar.props.max_value = 4.0
+        self.level_bar.props.mode = Gtk.LevelBarMode.DISCRETE
 
         # Values in a discrete mode are rounded
         # instead of using a floor function.
-        self.add_offset_value("insecure", 0.49)
-        self.add_offset_value("weak", 1.49)
-        self.add_offset_value("medium", 2.49)
-        self.add_offset_value("strong", 3.49)
-        self.add_offset_value("secure", 4.0)
+        self.level_bar.add_offset_value("insecure", 0.49)
+        self.level_bar.add_offset_value("weak", 1.49)
+        self.level_bar.add_offset_value("medium", 2.49)
+        self.level_bar.add_offset_value("strong", 3.49)
+        self.level_bar.add_offset_value("secure", 4.0)
+
+        self.set_child(self.level_bar)
 
     @GObject.Property(type=str, default="")
     def password(self) -> str:
@@ -35,6 +39,6 @@ class PasswordLevelBar(Gtk.LevelBar):
         self._password = password
 
         def on_password(strength):
-            self.props.value = strength
+            self.level_bar.props.value = strength
 
         strength_async(password, on_password)

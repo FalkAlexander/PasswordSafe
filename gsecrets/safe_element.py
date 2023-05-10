d87c5bd0cb7473c7715aab6d4ba9a5bfe323bbe3
Maximiliano Sandoval R <msandova@gnome.org>
2023-05-10 23:45:51 +0200

    safe_element: Fix typo cought -> caught

--- gsecrets/safe_element.py
+++ gsecrets/safe_element.py
@@ -808,7 +808,7 @@ class SafeEntry(SafeElement):
                 return self._otp.now()
             except binascii.Error:
                 logging.debug(
-                    "Error cought in OTP token generation (likely invalid "
+                    "Error caught in OTP token generation (likely invalid "
                     "base32 secret)."
                 )
 

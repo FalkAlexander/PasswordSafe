edff200a3587fc643650c2c870b1018080868fc8
Maximiliano Sandoval <msandova@gnome.org>
2025-08-12 21:58:50 +0200

    sorting: Use gtk dedicated property sorters
    Instead of CustomSorters which are less efficient.

--- gsecrets/sorting.py
+++ gsecrets/sorting.py
@@ -6,10 +6,9 @@ from __future__ import annotations
 import typing
 from enum import IntEnum
 
-from gi.repository import GLib, Gtk
+from gi.repository import Gtk
 
-if typing.TYPE_CHECKING:
-    from gsecrets.safe_element import SafeElement
+from gsecrets.safe_element import SafeElement
 
 
 class SortingHat:
@@ -21,64 +20,24 @@ class SortingHat:
         CTIME_ASC = 2
         CTIME_DEC = 3
 
-    # will be set from just below the class
-    sort_funcs: dict[SortOrder, typing.Callable] = {}
-
     @staticmethod
     def get_sorter(order: SortOrder) -> typing.Callable:
-        sort_func = SortingHat.sort_funcs[order]
-        return Gtk.CustomSorter.new(sort_func)
-
-    @staticmethod
-    def sort_by_name_asc(
-        ele1: SafeElement,
-        ele2: SafeElement,
-        _data: object = None,
-    ) -> int:
-        return GLib.ascii_strcasecmp(ele1.name, ele2.name)
-
-    @staticmethod
-    def sort_by_name_dec(
-        ele1: SafeElement,
-        ele2: SafeElement,
-        _data: object = None,
-    ) -> int:
-        return GLib.ascii_strcasecmp(ele2.name, ele1.name)
-
-    @staticmethod
-    def sort_by_ctime_asc(
-        ele1: SafeElement,
-        ele2: SafeElement,
-        _data: object = None,
-    ) -> int:
-        if ele1.ctime is not None and ele2.ctime is not None:
-            return ele1.ctime.compare(ele2.ctime)
-        if ele1.ctime is None and ele2.ctime is not None:
-            return 1
-        if ele1.ctime is not None and ele2.ctime is None:
-            return -1
-
-        return 0
-
-    @staticmethod
-    def sort_by_ctime_dec(
-        ele1: SafeElement,
-        ele2: SafeElement,
-        _data: object = None,
-    ) -> int:
-        if ele1.ctime is not None and ele2.ctime is not None:
-            return ele2.ctime.compare(ele1.ctime)
-        if ele1.ctime is None and ele2.ctime is not None:
-            return -1
-        if ele1.ctime is not None and ele2.ctime is None:
-            return 1
-
-        return 0
-
-
-SortingHat.sort_funcs = {
-    SortingHat.SortOrder.ASC: SortingHat.sort_by_name_asc,
-    SortingHat.SortOrder.DEC: SortingHat.sort_by_name_dec,
-    SortingHat.SortOrder.CTIME_ASC: SortingHat.sort_by_ctime_asc,
-    SortingHat.SortOrder.CTIME_DEC: SortingHat.sort_by_ctime_dec,
-}
+        match order:
+            case SortingHat.SortOrder.ASC:
+                expr = Gtk.PropertyExpression.new(SafeElement.__gtype__, None, "name")
+                return Gtk.StringSorter.new(expr)
+            case SortingHat.SortOrder.DEC:
+                expr = Gtk.PropertyExpression.new(SafeElement.__gtype__, None, "name")
+                return Gtk.StringSorter.new(expr)
+            case SortingHat.SortOrder.CTIME_ASC:
+                expr = Gtk.PropertyExpression.new(
+                    SafeElement.__gtype__, None, "ctime_int"
+                )
+                return Gtk.NumericSorter.new(expr)
+            case SortingHat.SortOrder.CTIME_DEC:
+                expr = Gtk.PropertyExpression.new(
+                    SafeElement.__gtype__, None, "ctime_int"
+                )
+                sorter = Gtk.NumericSorter.new(expr)
+                sorter.props.sort_order = Gtk.SortType.DESCENDING
+                return sorter
